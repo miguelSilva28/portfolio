ef79ef08bc17c150ea23766ec62b9494bc47ff7b
Miguel da Silva Assis <miguelassis28@gmail.com>
2023-12-12 16:11:05 +0000

    ajustes finais

--- js/api.js
+++
@@ -1,5 +0,0 @@
-async function fetchProfileData() {
-    const url = 'https://raw.githubusercontent.com/miguelSilva28/portfolio/main/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
