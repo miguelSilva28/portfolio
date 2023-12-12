a023412456e150e271eff9311c56c3580de98dfd
Miguel da Silva Assis <miguelassis28@gmail.com>
2023-12-12 16:50:14 +0000

    edição mainjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,11 +41,12 @@ function updatePortfolio(profileData) {
     const portfolio = document.getElementById('profile.portfolio')
     portfolio.innerHTML = profileData.portfolio.map(project => {
         return `
-            <li>
-                <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
-                <a href="${project.url}" target="_blank">${project.url}</a>
-            </li>
+        <li>
+            <h3>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
         `
+
     }).join('')
 }
 
