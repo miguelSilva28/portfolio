0627041a03c60d70d1fd81b478119a4d73892a57
Miguel da Silva Assis <miguelassis28@gmail.com>
2023-12-12 16:36:22 +0000

    alteração da requisição da api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,6 @@
 async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/miguelSilva28/portfolio/main/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
+    const response = await fetch(url)
+    const profileData = await response.json()
+    return profileData
 }
